3291c7a688f1885491ec2df1287c4d5e040f687e
Naruto2627 <64784139+Naruto2627@users.noreply.github.com>
2020-05-25 13:47:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,25 +16,34 @@ function setup() {
 function draw() {
   car.velocityX = speed;
 
-  if(wall.x-car.x(car.width+wall.width)/2)
+  if(wall.x-car.x < (car.width+wall.width)/2)
   {
     car.velocityX=0;
 
     var deformation = 0.5 * weight * speed * speed/22509;
     
-    if(deformation>100)
+    if(deformation>180)
     {
-      car.shapeColor = color(255,0,0);
+      car.shapeColor = color(255,0,0); 
+
+      
+      Text("This car is lethal!",50,100);
     }
 
     if(deformation<180 && deformation>100)
     {
       car.shapeColor=color(230,230,0);
+
+      Text("This car is ok for passengers!",50,100);
+
     }
 
     if(deformation<100)
     {
       car.shapeColor=color(0,255,0);
+
+      Text("The car is !",50,100);
+
     }
   }
 
