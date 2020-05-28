55e3e8bc0eb06766fbf14140b18e7cc693a73d65
Naruto2627 <64784139+Naruto2627@users.noreply.github.com>
2020-05-28 21:03:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,39 @@
-var car,wall
-var speed,weight
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function setup() {
-  createCanvas(1600,400);
+var engine, world;
+var ground,ball;
 
-  car = createSprite(50, 200, 50, 50);
-  wall = createSprite(1500,200,60,height/2);
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
 
-  
+    var ground_options ={
+        isStatic: true
+    }
 
-  speed = random(55,90);
-  weight = random (400,1500);
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options={
+        restitution: 1.0
+    }
+
+    ball = Bodies.circle(200,100,20,ball_options);
+    World.add(world,ball);
+
+    console.log(ground);
 }
 
-function draw() {
-  car.velocityX = speed;
-
-  if(wall.x-car.x < (car.width+wall.width)/2)
-  {
-
-    var deformation = 0.5 * weight * speed * speed/22509;
-
-  if(car.isTouching(wall)){
-    car.velocityX = 0;
-  }
+function draw(){
+    background(0);
+    Engine.update(engine);
     
-    if(deformation<100)
-    {
-      car.shapeColor = color(255,0,0); 
-    }
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
 
-    if(deformation<180 && deformation>100)
-    {
-      car.shapeColor=color(230,230,0);
-    }
-
-    if(deformation>180)
-    {
-      car.shapeColor=color(0,255,0);
-    }
-  }
-
-  /*if(car.isTouching(wall) && car.shapeColor(255,0,0)){
-    Text("This car is very good for passengers ");
-  }
-  else if (car.isTouching(wall) && car.shapeColor(230,230,0)){
-  Text("This car is average i.e its safe for passengers");
-  }
-  else(car.isTouching(wall) && car.shapeColor(0,225,0)){
-  Text("This car is lethal for passengers");
-  }*/
-
-
-  background(255,255,255);
-
-  drawSprites();
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x,ball.position.y,20,20);
 }
